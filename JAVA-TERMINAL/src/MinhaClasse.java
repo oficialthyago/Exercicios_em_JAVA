60bd5d059a6cf3070b348963cd6063242b9a3f82
Thyago <ofcialthyago2@gmail.com>
2023-04-08 00:32:27 -0300

    projeto com inserção de dados do user final

---
+++ JAVA-TERMINAL/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.println("oi, fui executado pelo terminal");
+        
+    }
+        
+    }
+    
--- sistema_smart_tv/src/Usuario.java
+++ sistema_smart_tv/src/Usuario.java
@@ -2,6 +2,7 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
         
+        /*Sistema contador de canais */
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
